d4ed6ad2d688f1fcddd750e2afbf41f1e4648cca
mlip <kiwaczki@gmail.com>
2018-03-16 13:18:15 +0100

    MongoLab connection details taken from environment

--- src/main/java/com/github/mpi/infrastructure/mongo/MongoContext.java
+++ src/main/java/com/github/mpi/infrastructure/mongo/MongoContext.java
@@ -1,18 +1,12 @@
 package com.github.mpi.infrastructure.mongo;
 
-import com.google.common.collect.Lists;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
-import org.springframework.beans.factory.annotation.Value;
+import com.mongodb.MongoClientURI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
-
-import java.util.List;
 
 @Configuration
 @Profile("mongo")
@@ -24,20 +18,8 @@ public class MongoContext {
     }
 
     @Bean
-    protected MongoDbFactory mongoDbFactory(
-            @Value("${mongo.db.url}") String host, 
-            @Value("${mongo.db.port}") int port,
-            @Value("${mongo.db.name}") String databaseName, 
-            @Value("${mongo.db.user}") String username, 
-            @Value("${mongo.db.pass}") String password) throws Exception {
-        
-        return new SimpleMongoDbFactory(new MongoClient(new ServerAddress(host, port), getCredentials(username, databaseName, password)), databaseName);
+    protected MongoDbFactory mongoDbFactory(MongoClientURI mongoDbURI) throws Exception {
+        return new SimpleMongoDbFactory(mongoDbURI);
     }
 
-    private List<MongoCredential> getCredentials(String user, String databaseName, String password) {
-        if(user.length() == 0) {
-            return Lists.newArrayList();
-        }
-        return Lists.newArrayList(MongoCredential.createCredential(user, databaseName, password.toCharArray()));
-    }
 }
